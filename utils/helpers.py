265ba3bfe5f3c5584dc45c8357720e43385e11b4
Ty <168314093+tyreseGill@users.noreply.github.com>
2026-04-27 13:46:36 -0700

    fix: handle list-valued WHOIS expiration dates

--- utils/helpers.py
+++ utils/helpers.py
@@ -21,8 +21,15 @@ def validate_certificate(query: dict) -> bool:
         bool: True if the certificate for the domain has not yet expired. Otherwise, false.
     """
     # Initializes date variables with global timezone standard
-    cert_expiration_date = query.expiration_date.astimezone(tz.utc)
+    cert_expiration_date = query.expiration_date
     current_date = dt.now(tz.utc)
+    
+    # Fetches earliest expiration date if multiple expiration dates exist
+    if isinstance(cert_expiration_date, list):
+        cert_expiration_date = min(cert_expiration_date)
+    
+    # Converts expiration date to standard timezone for comparison
+    cert_expiration_date = cert_expiration_date.astimezone(tz.utc)
 
     # Determines if certificate is expired or not
     if cert_expiration_date < current_date:
