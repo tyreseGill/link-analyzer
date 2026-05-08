c40e570239616969168bbda6bd3ffbcf62a5a508
Ty <168314093+tyreseGill@users.noreply.github.com>
2026-05-08 16:42:02 -0700

    feat: extend Certificate model with issuer CN, age, and self-signed detection

--- utils/cert_utils.py
+++ utils/cert_utils.py
@@ -11,8 +11,9 @@ class Certificate:
         subject = dict(x[0] for x in cert['subject'])
         issuer  = dict(x[0] for x in cert['issuer'])
 
-        self.common_name = subject['commonName']
-        self.issuer_name = issuer['organizationName']
+        self.subject_cn = subject['commonName']
+        self.issuer_org_name = issuer['organizationName']
+        self.issuer_cn = issuer['commonName']
         self.version  = cert['version']
         self.not_before = datetime.strptime(
             cert['notBefore'], "%b %d %H:%M:%S %Y %Z"
@@ -25,11 +26,16 @@ class Certificate:
         ]
 
     def __repr__(self):
-        return f'Common Name: {self.common_name}, Issuer: {self.issuer}, Issuer Name: {self.issuer_name}, Version: {self.version}, Valid Between: {self.not_before} thru {self.not_after}, SANS: {self.sans}'
+        return f'Common Name: {self.subject_cn}, Issuer Org: {self.issuer_org_name}, Issuer CN: {self.issuer_cn}, Version: {self.version}, Valid Between: {self.not_before} thru {self.not_after}, SANS: {self.sans}'
 
     def is_valid(self):
         current_time = datetime.now(tz.utc)
         return self.not_before <= current_time <= self.not_after
+    
+    def get_age(self):
+        current_time = datetime.now(tz.utc).date()
+        age_days = (current_time - self.not_before.date()).days
+        return age_days
 
 
 def get_tls_certificate(url: str) -> Certificate:
@@ -74,3 +80,18 @@ def verify_hostname(cert: Certificate, url: str) -> bool:
     match_found = any(san.replace('*.', '') in hostnames for san in cert.sans)
 
     return match_found
+
+
+def is_self_signed(cert: Certificate) -> bool:
+    """
+    Performs check to verify whether a certificate was self-signed or not.
+
+    Args:
+        cert: Certificate to check.
+    
+    Returns:
+        bool: True, if the subject and issuer of the certificate are the same. Else, false.
+    """
+    subject_cn = cert.subject_cn
+    issuer_cn  = cert.issuer_cn
+    return True if subject_cn == issuer_cn else False
