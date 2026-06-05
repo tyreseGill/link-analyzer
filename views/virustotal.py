ac38e061b2bec4d31e0540bc9066d096452facc3
Ty <168314093+tyreseGill@users.noreply.github.com>
2026-06-05 16:54:51 -0700

    refactor: modularize  function

--- views/virustotal.py
+++ views/virustotal.py
@@ -12,40 +12,54 @@ def print_virus_total_stats(url: str):
         print("\n[INFO] Failed to fetch statistics within alloted time.")
         return
         
-    num_undetected = stats["undetected"]
-    num_harmless = stats["harmless"]
-    num_suspicious = stats["suspicious"]
-    num_malicious = stats["malicious"]
-    num_total = sum(
-        [num_undetected, num_harmless, num_suspicious, num_malicious]
-    )
-
-    num_undetected = (
-        highlight_red(num_undetected)
-        if num_undetected == 0 and num_harmless == 0
-        else highlight_green(num_undetected)
-    )
-    num_harmless = (
-        highlight_red(num_harmless)
-        if num_harmless == 0
-        else highlight_green(num_harmless)
-    )
-
-    if num_suspicious == 0:
-        num_suspicious = highlight_green(num_suspicious)
-    elif num_suspicious > (0.5 * num_total):
-        num_suspicious = highlight_red(num_suspicious)
-    else:
-        num_suspicious = highlight_yellow(num_suspicious)
-        
-    if num_malicious == 0:
-        num_malicious = highlight_green(num_malicious)
-    elif num_malicious > (0.5 * num_total):
-        num_malicious = highlight_red(num_malicious)
-    else:
-        num_malicious = highlight_yellow(num_malicious)
+    num_undetected, num_harmless, num_suspicious, num_malicious = get_stats(stats)
+    num_undetected, num_harmless, num_suspicious, num_malicious = highlight_stats(stats)
     
     print_kv("Undetected", num_undetected)
     print_kv("Harmless", num_harmless)
     print_kv("Suspicious", num_suspicious)
     print_kv("Malicious", num_malicious)
+
+
+def get_stats(stats: dict):
+    num_undetected = stats["undetected"]
+    num_harmless = stats["harmless"]
+    num_suspicious = stats["suspicious"]
+    num_malicious = stats["malicious"]
+
+    return num_undetected, num_harmless, num_suspicious, num_malicious
+
+
+def highlight_stats(stats: dict):
+    num_undetected, num_harmless, num_suspicious, num_malicious = get_stats(stats)
+
+    num_total = sum(
+        [num_undetected, num_harmless, num_suspicious, num_malicious]
+    )
+
+    num_undetected = highlight_stable_stats(num_undetected, num_harmless)
+    num_harmless = highlight_stable_stats(num_harmless, num_undetected)
+    num_suspicious = highlight_critical_stats(num_suspicious, num_total)
+    num_malicious = highlight_critical_stats(num_malicious, num_total)
+
+    return num_undetected, num_harmless, num_suspicious, num_malicious
+
+
+def highlight_stable_stats(stat: int, other_stat: int):
+    color = (
+        highlight_red(stat)
+        if stat == 0 and other_stat == 0
+        else highlight_green(stat)
+    )
+    return color
+
+
+def highlight_critical_stats(stat: int, num_total: int):
+    if stat == 0:
+        color = highlight_green(stat)
+    elif stat > (0.5 * num_total):
+        color = highlight_red(stat)
+    else:
+        color = highlight_yellow(stat)
+    
+    return color
