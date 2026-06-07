aa01b7bc1d14b1b35dcede6cbd81a1b3c4321a01
Ty <168314093+tyreseGill@users.noreply.github.com>
2026-06-07 14:11:21 -0700

    refactor: consolidate print_url_info logic - Deleted `print_url_info` and moved logic into `print_url_struct_analysis`

--- views/url.py
+++ views/url.py
@@ -4,13 +4,5 @@ from views.helpers import print_header, print_kv
 
 def print_url_struct_analysis(risk: dict):
     print_header("URL Structure Analysis")
-    print_url_info(risk)
-
-    
-def print_url_info(risk: dict):
     color_coded_url = risk["url_structure"]["rendered_url"]
     print_kv("Analyzed URL", color_coded_url)
-    print("\nLegend:\n" \
-    f"\t{highlight_red("RED")} = High Risk Indicator\n" \
-    f"\t{highlight_yellow("YELLOW")} = Suspicious structure or keyword\n" \
-    f"\t{highlight_green("GREEN")} = Expected / secure component")
